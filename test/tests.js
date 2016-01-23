ec88fc28ed7c1bb6a8e2f3fc44a6354c2795541b
Martin Imfeld <martin.imfeld@gmail.com>
2016-01-23 09:28:47 +0100

    tests added

---
+++ test/tests.js
@@ -0,0 +1,32 @@
+const expect = require('chai').expect
+const should = require('chai').should()
+const set = require(__dirname + '/../index.js').set
+const settings = require(__dirname + '/../index.js').settings
+
+describe('All', () => {
+
+  it("One level", () => {
+    // arrange / act
+    set('name', 'Alice')
+    set('age', 16)
+    // assert
+    expect(settings.name).to.equal('Alice')
+    expect(settings.age).to.equal(16)
+  })
+
+  it("Two Levels", () => {
+    // arrange / act
+    set('book.title', 'Through the Looking-Glass')
+    // assert
+    expect(settings.book.title).to.equal('Through the Looking-Glass')
+  })
+
+  it("Several Items at the same level", () => {
+    // arrange
+    set('book.author', 'Lewis Carroll')
+    set('book.published', 1871)
+    // assert
+    expect(settings.book.author).to.equal('Lewis Carroll')
+    expect(settings.book.published).to.equal(1871)
+  })
+})
